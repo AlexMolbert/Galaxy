d50dfced7b7a40b76af42175485d63875c45e4e2
Alexander Molbert <alexander.molbert@e-hps.com>
2020-08-19 09:16:38 -0500

    Skeleton web App

---
+++ Galaxy/GalaxyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GalaxyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
